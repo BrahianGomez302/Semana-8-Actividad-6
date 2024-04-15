cb0615a8a7b1cb7bb76df7c4de6c85ae59620cb2
Brahian Gomez <estebangomez302@hotmail.com>
2024-04-15 18:46:54 -0500

    ejercicio 1

--- ejercicio2/src/main/java/com/example/Main.java
+++ ejercicio2/src/main/java/com/example/Main.java
@@ -1,7 +1,35 @@
 package com.example;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        adivinarNumero(1, 100, 5);
     }
-}
+    public static void adivinarNumero(int min_numero, int max_numero, int max_intentos) {
+       //Implementar solución
+       Random rand = new Random();
+        int numeroAleatorio = rand.nextInt(max_numero - min_numero + 1) + min_numero;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bienvenido al juego de adivinar el número");
+        System.out.println("Se ha generado un número aleatorio entre " + min_numero + " y " + max_numero + ". Tienes " + max_intentos + " intentos para adivinarlo.");
+
+        for (int intento = 1; intento <= max_intentos; intento++) {
+            System.out.print("Intento #" + intento + ": Introduce un numero: ");
+            int suposicion = scanner.nextInt();
+
+            if (suposicion == numeroAleatorio) {
+                System.out.println("Felicidades Has adivinado el número en " + intento + " intentos");
+                return;
+            } else {
+                if (suposicion < numeroAleatorio) {
+                    System.out.println("El número aleatorio es mayor.");
+                } else {
+                    System.out.println("El número aleatorio es menor.");
+                }
+            }
+        }
+        System.out.println("Lo siento, ya no quedan mas intentos, El número aleatorio era: " + numeroAleatorio);
+    }
+    }
